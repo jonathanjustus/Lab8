91e3aad7971cf0c4239d068ff35a8449621034d3
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-26 20:28:27 -0500

    ToDo Class

---
+++ Todo.java
@@ -0,0 +1,33 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-03-26
+OU ID_______________: 113480781
+OU EMAIL____________: jonathan.justus@ou.edu
+
+Tasks:
+1. BoardGame Class
+2. Color Class
+3. GamePiece Class
+4. GamePieceAppearance Class
+5. Location Class
+6. Shape Class
+7. Testing & Tweaking
+
+Estimated Time (minutes):
+1. 50
+2. 20
+3. 45
+4. 20
+5. 10
+6. 15
+7. 90
+
+Actual Time (minutes):
+1. 60
+2. 20
+3. 60
+4. 20
+5. 5
+6. 15
+7. 120
+*/}
